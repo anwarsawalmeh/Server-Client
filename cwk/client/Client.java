ff06fb0bfcfde72fb8abf11b710dc90cdc5e260c
Anwar Sawalmeh <sc212as@leeds.ac.uk>
2023-03-09 13:56:49 +0000

    Started working on command line arguments

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -3,28 +3,37 @@ import java.net.*;
 
 public class Client 
 {
-	public void connect(){
-
-		try
-		{
+	public void connect(String command, String item, int bid){
+		System.out.println(command+" "+" "+item+" "+ bid);
+		if(command == "show"){
+			try{
 			Socket cSocket = new Socket("localhost", 6000);
 			BufferedReader reader = new BufferedReader(
 										new InputStreamReader(
-											cSocket.getInputStream() ) );
+											cSocket.getInputStream()));
 			String message = reader.readLine();
 			System.out.println(message);
 			cSocket.close();
-		}
-		catch(IOException e){
-			System.out.println(e);
+			}catch(IOException e){
+				System.out.println(e);
+			}
+			
 		}
 		
+		
 	}
-
 
 	public static void main( String[] args )
 	{
 		Client client = new Client();
-		client.connect();
+		if(args.length == 1){
+			client.connect(args[0], null, -1);
+		}
+		else if(args.length == 2){
+			client.connect(args[0], args[1], -1);
+		}
+		else if(args.length == 3){
+			client.connect(args[0], args[1], Integer.valueOf(args[2]));
+		}
 	}
 }
--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -28,7 +28,6 @@ public class Server
         }
     }
 
-
 	public static void main( String[] args )
 	{
         Server server = new Server();
