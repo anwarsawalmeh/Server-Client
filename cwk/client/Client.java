a32841ecdabb2df499bb76bcb53cb80fe81a6028
Anwar Sawalmeh <sc212as@leeds.ac.uk>
2023-03-09 11:58:07 +0000

    Basic Client Connecting to server

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -1,6 +1,28 @@
+import java.io.*;
+import java.net.*;
+
 public class Client 
 {
+	public void connect(){
+
+		try
+		{
+			Socket cSocket = new Socket("localhost", 6000);
+			BufferedReader reader = new BufferedReader(
+										new InputStreamReader(
+											cSocket.getInputStream() ) );
+			cSocket.close();
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		
+	}
+
+
 	public static void main( String[] args )
 	{
+		Client client = new Client();
+		client.connect();
 	}
 }
